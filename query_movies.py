5bc188adae1daa750557c931901c6bb079a74afc
Ebenezer Anyanwu <ebenezer.anyanwu83@gmail.com>
2026-04-19 13:49:06 -0400

    final version with analysis queries

--- query_movies.py
+++ query_movies.py
@@ -4,11 +4,29 @@ conn = sqlite3.connect('movies.db')
 cursor = conn.cursor()
 
 cursor.execute("""
-SELECT * FROM movies
-WHERE year > 1990
+SELECT title, rating
+FROM movies
 ORDER BY rating DESC
+LIMIT 5
 """)
 
+print("Top 5 movies:")
+for row in cursor.fetchall():
+    print(row)
+
+
+cursor.execute("SELECT AVG(rating) FROM movies")
+
+print("\nAverage rating:")
+print(cursor.fetchone())
+
+cursor.execute("""
+SELECT title, year
+FROM movies
+WHERE year > 2000
+""")
+
+print("\nMovies after 2000:")
 for row in cursor.fetchall():
     print(row)
 
