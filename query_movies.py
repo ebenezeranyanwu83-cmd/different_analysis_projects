d9f78766aaa25d6a2c556daca901568351a3e32f
ebenezeranyanwu83-cmd <ebenezer.anyanwu83@gmail.com>
2026-04-25 10:06:10 -0400

    Add files via upload

--- query_movies.py
+++ query_movies.py
@@ -4,29 +4,11 @@ conn = sqlite3.connect('movies.db')
 cursor = conn.cursor()
 
 cursor.execute("""
-SELECT title, rating
-FROM movies
+SELECT * FROM movies
+WHERE year > 1990
 ORDER BY rating DESC
-LIMIT 5
 """)
 
-print("Top 5 movies:")
-for row in cursor.fetchall():
-    print(row)
-
-
-cursor.execute("SELECT AVG(rating) FROM movies")
-
-print("\nAverage rating:")
-print(cursor.fetchone())
-
-cursor.execute("""
-SELECT title, year
-FROM movies
-WHERE year > 2000
-""")
-
-print("\nMovies after 2000:")
 for row in cursor.fetchall():
     print(row)
 
--- setup_db.py
+++ setup_db.py
@@ -17,17 +17,7 @@ rating REAL
 movies_list = [
     ("Inception", 2010, 8.8),
     ("Titanic", 1997, 7.9),
-    ("The Matrix", 1999, 8.7),
-    ("Ocean's Eleven", 2001, 7.7),
-    ("Gladiator" , 2000, 8.5),
-    ("Toy Story 2", 1999, 7.9),
-    ("Blade", 1998, 7.1),
-    ("Face/Off", 1997, 7.3),
-    ("Men In Black", 1997, 7.3),
-    ("Rush Hour", 1998, 7.0),
-    ("X-Men", 2000, 7.3),
-    ("Shrek", 2001, 7.9)
-
+    ("The Matrix", 1999, 8.7)
 ]
 
 cursor.executemany("INSERT INTO movies (title, year, rating) VALUES (?, ?, ?)", movies_list)
